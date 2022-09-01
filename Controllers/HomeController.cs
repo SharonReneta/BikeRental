bff7d8806fe80a82d83e13e1f38a76b7dc7fff3a
sharon <sharon@KARO_LT18>
2022-09-01 18:32:04 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BikeRental.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/textpage1")]
+        public IActionResult HOME()
+        {
+            return View();
+        }
+
+
+    }
+}
